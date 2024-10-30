8cb05109cb6fa51cbf671a4c00410d3eaffe7cca
Misael10x <misabar12389@gmail.com>
2024-10-29 20:47:51 -0600

    update

--- Puzzle_lineal.py
+++ Puzzle_lineal.py
@@ -14,3 +14,27 @@ def buscar_solucion_BFS(estado_inicial, solucion):
             return nodo
         else:
             dato_nodo = nodo.get_datos()
+
+
+            # Operador Izquierdo
+            hijo = [dato_nodo[1], dato_nodo[0], dato_nodo[2], dato_nodo[3]]
+            hijo_izquierdo = Nodo(hijo, nodo)
+
+            if not hijo_izquierdo.en_lista(nodos_visitados) and not hijo_izquierdo.en_lista(nodos_frontera):
+                nodos_frontera.append(hijo_izquierdo)
+
+            # Operador central 
+            hijo = [dato_nodo[0], dato_nodo[2], dato_nodo[1], dato_nodo[3]]
+            hijo_central = Nodo(hijo, nodo)
+
+            if not hijo_central.en_lista(nodos_visitados) and not hijo_central.en_lista(nodos_frontera):
+                nodos_frontera.append(hijo_central)
+
+            # Operador Derecho
+            hijo = [dato_nodo[0], dato_nodo[1], dato_nodo[3], dato_nodo[2]]
+            hijo_derecho = Nodo(hijo, nodo)
+
+            if not hijo_derecho.en_lista(nodos_visitados) and not hijo_derecho.en_lista(nodos_frontera):
+            
+            
+                nodos_frontera.append(hijo_derecho)
